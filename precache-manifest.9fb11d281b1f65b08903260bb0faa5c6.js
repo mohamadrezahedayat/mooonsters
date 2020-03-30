39a16fe6d848cfeb7d945548b431a1227f071cb0
MohamadReza Hedayatnia <mohamadrezahedayat@gmail.com>
2020-03-30 04:52:08 +0430

    Updates

---
+++ precache-manifest.9fb11d281b1f65b08903260bb0faa5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a76f95bf25f19c55d2f5c6e8241503",
+    "url": "/Monsters-Factory/index.html"
+  },
+  {
+    "revision": "3cf97019ba2a30df1e8e",
+    "url": "/Monsters-Factory/static/css/main.384f3259.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/Monsters-Factory/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Factory/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf97019ba2a30df1e8e",
+    "url": "/Monsters-Factory/static/js/main.291dc289.chunk.js"
+  },
+  {
+    "revision": "7ce12ef457b5d780d82a",
+    "url": "/Monsters-Factory/static/js/runtime-main.0418103e.js"
+  }
+]);
